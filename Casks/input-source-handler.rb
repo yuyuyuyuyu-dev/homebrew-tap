fe7f9cf49088f56dbf18b6f189937a4a1aac016c
yu <yu@yuyuyuyuyu.dev>
2026-04-19 11:37:19 +0900

    feat(input-source-handler): add InputSourceHandler cask
    Add a new Cask for InputSourceHandler version 0.1.0 to enable installation via Homebrew.

---
+++ Casks/input-source-handler.rb
@@ -0,0 +1,10 @@
+cask "input-source-handler" do
+  version "0.1.0"
+  sha256 "91a7cf8b71c14ce652122466a89c87cd52124ae963f0887179c4baaa4bde2d7e"
+
+  url "https://github.com/yuyuyuyuyu-dev/input-source-handler/releases/download/v#{version}/InputSourceHandler.zip"
+  name "Input Source Handler"
+  homepage "https://github.com/yuyuyuyuyu-dev/input-source-handler#readme"
+
+  app "InputSourceHandler.app"
+end
